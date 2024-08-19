2a76d703847073988fc34546101c63c333854c24
Diego Mogollon <55108265+diego-mogollon@users.noreply.github.com>
2024-08-19 22:25:05 +1000

    Completed folder structure setup, created lateguru package, setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+#setup.py
+from setuptools import setup
+from setuptools import find_packages
+
+#standard pytthon code for requirements.txt file
+with open('requirements.txt') as f:
+    content = f.readlines()
+    requirements = [x.strip() for x in content]
+
+#standard python code for packages logic
+setup(name='lateguru', #package name
+      description='predict flight delays', #optional description
+      install_requires=requirements, #installs packages from requirements.txt
+      packages=find_packages()) #contanstly looks for any new packages in our project, but our main is 'lateguru_ml'
